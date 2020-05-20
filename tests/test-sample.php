32fc869941d6e11aff14ea9fb6d2caa590cf326f
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2020-05-20 09:43:02 +0800

    Add unit test

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Image_Optimizer_By_Wenprise
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
